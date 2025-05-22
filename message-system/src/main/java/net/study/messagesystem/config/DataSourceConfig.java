e491a556305ddc952df45c17d0e84088b810987f
koviKsw <seonwongim@gmail.com>
2025-05-22 13:09:49 +0900

    히카리 DataSource 설정 빈 등록

---
+++ message-system/src/main/java/net/study/messagesystem/config/DataSourceConfig.java
@@ -0,0 +1,19 @@
+package net.study.messagesystem.config;
+
+import com.zaxxer.hikari.HikariDataSource;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.jdbc.DataSourceBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import javax.sql.DataSource;
+
+@Configuration
+public class DataSourceConfig {
+
+    @Bean
+    @ConfigurationProperties(prefix = "spring.datasource.hikari")
+    public DataSource dataSource() {
+        return DataSourceBuilder.create().type(HikariDataSource.class).build();
+    }
+}
