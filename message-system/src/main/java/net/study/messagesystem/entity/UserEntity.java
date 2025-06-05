936fd1acdf54d280794dfed25694d273c9caeffd
koviKsw <seonwongim@gmail.com>
2025-06-05 12:49:24 +0900

    유저 엔티티 및 리포지토리 생성

---
+++ message-system/src/main/java/net/study/messagesystem/entity/UserEntity.java
@@ -0,0 +1,61 @@
+package net.study.messagesystem.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.ToString;
+
+import java.time.LocalDateTime;
+import java.util.Objects;
+
+@Getter
+@ToString
+@Entity
+@Table(name = "user")
+public class UserEntity {
+
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "user_id", nullable = false)
+    private Long userId;
+
+    @Column(name = "user_name", nullable = false)
+    private String username;
+
+    @Column(name = "password", nullable = false)
+    private String password;
+
+    @Column(name = "created_at", nullable = false)
+    private LocalDateTime createdAt;
+
+    @Column(name = "updated_at", nullable = false)
+    private LocalDateTime updatedAt;
+
+    public UserEntity() {}
+
+    public UserEntity(String username, String password) {
+        this.username = username;
+        this.password = password;
+    }
+
+    @PrePersist
+    public void prePersist() {
+        createdAt = LocalDateTime.now();
+        updatedAt = createdAt;
+    }
+
+    @PreUpdate
+    public void preUpdate() {
+        updatedAt = LocalDateTime.now();
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        UserEntity that = (UserEntity) o;
+        return Objects.equals(userId, that.userId);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(userId);
+    }
+}
--- message-system/src/main/resources/schema.sql
+++ message-system/src/main/resources/schema.sql
@@ -6,3 +6,13 @@ CREATE TABLE IF NOT EXISTS message (
     updated_at TIMESTAMP NOT NULL,
     PRIMARY KEY (message_sequence)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+CREATE TABLE IF NOT EXISTS user (
+    user_id BIGINT AUTO_INCREMENT,
+    user_name VARCHAR(20) NOT NULL,
+    password VARCHAR(50) NOT NULL,
+    created_at TIMESTAMP NOT NULL,
+    updated_at TIMESTAMP NOT NULL,
+    PRIMARY KEY (user_id)
+    CONSTRAINT unique_user_name UNIQUE (user_name)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
