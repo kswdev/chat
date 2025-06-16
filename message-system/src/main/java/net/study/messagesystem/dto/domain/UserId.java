62918ca9006195c09ead86a36e534ac60c5e7b01
koviKsw <seonwongim@gmail.com>
2025-06-16 12:54:59 +0900

    userId 키 클래스 생성

---
+++ message-system/src/main/java/net/study/messagesystem/dto/domain/UserId.java
@@ -0,0 +1,9 @@
+package net.study.messagesystem.dto.domain;
+
+public record UserId(Long id) {
+
+    public UserId {
+        if (id == null || id < 0)
+            throw new IllegalArgumentException("Invalid UserId");
+    }
+}
