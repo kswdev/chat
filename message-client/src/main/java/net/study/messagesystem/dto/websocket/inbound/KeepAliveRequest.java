30b9c126c5c8f501dba1b3639cced94342fabb1f
ksw <seonwongim@gmail.com>
2025-07-09 21:38:50 +0900

    [message-client] - 역직렬화 하는제 필요한 어노테이션이므로 삭제

--- message-client/src/main/java/net/study/messagesystem/dto/websocket/inbound/KeepAliveRequest.java
+++ message-client/src/main/java/net/study/messagesystem/dto/websocket/inbound/KeepAliveRequest.java
@@ -1,11 +1,9 @@
 package net.study.messagesystem.dto.websocket.inbound;
 
-import com.fasterxml.jackson.annotation.JsonCreator;
 import net.study.messagesystem.constant.MessageType;
 
 public class KeepAliveRequest extends BaseRequest {
 
-    @JsonCreator
     public KeepAliveRequest() {
         super(MessageType.KEEP_ALIVE);
     }
--- message-client/src/main/java/net/study/messagesystem/dto/websocket/inbound/MessageRequest.java
+++ message-client/src/main/java/net/study/messagesystem/dto/websocket/inbound/MessageRequest.java
@@ -1,7 +1,5 @@
 package net.study.messagesystem.dto.websocket.inbound;
 
-import com.fasterxml.jackson.annotation.JsonCreator;
-import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Getter;
 import net.study.messagesystem.constant.MessageType;
 
@@ -11,10 +9,9 @@ public class MessageRequest extends BaseRequest {
     private final String username;
     private final String content;
 
-    @JsonCreator
     public MessageRequest(
-            @JsonProperty("username") String username,
-            @JsonProperty("content") String content
+            String username,
+            String content
     ) {
         super(MessageType.MESSAGE);
         this.username = username;
