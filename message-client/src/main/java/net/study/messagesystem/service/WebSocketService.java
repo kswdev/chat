bff4fe5905aa73d754bb9d7ef3c1fd96c86357d2
ksw <seonwongim@gmail.com>
2025-07-10 23:46:33 +0900

    [message-client] - WebSocketService 1. 세션 생성 후 웹소켓 엔드포인트 설정 헤더에 세션 추가 2. 세션 생성 후 1분 마다 연장 요청 보내는 스케쥴러 추가 3. isSessionOpen 공통 메서드 추출

--- message-client/src/main/java/net/study/messagesystem/service/WebSocketService.java
+++ message-client/src/main/java/net/study/messagesystem/service/WebSocketService.java
@@ -1,39 +1,68 @@
 package net.study.messagesystem.service;
 
+import jakarta.websocket.ClientEndpointConfig;
 import jakarta.websocket.CloseReason;
+import jakarta.websocket.SendHandler;
 import jakarta.websocket.Session;
-import net.study.messagesystem.dto.domain.Message;
+import net.study.messagesystem.dto.websocket.inbound.BaseRequest;
+import net.study.messagesystem.dto.websocket.inbound.KeepAliveRequest;
+import net.study.messagesystem.dto.websocket.inbound.MessageRequest;
 import net.study.messagesystem.handler.WebSocketMessageHandler;
 import net.study.messagesystem.handler.WebSocketSender;
+import net.study.messagesystem.handler.WebSocketSessionHandler;
+import net.study.messagesystem.util.JsonUtil;
 import org.glassfish.tyrus.client.ClientManager;
 
 import java.io.IOException;
 import java.net.URI;
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
 
 public class WebSocketService {
 
+    private final WebSocketMessageHandler webSocketMessageHandler;
     private final TerminalService terminalService;
     private final WebSocketSender webSocketSender;
     private final String websocketUrl;
 
-    private WebSocketMessageHandler webSocketMessageHandler;
+    private ScheduledExecutorService executorService;
+
     private Session session;
 
-    public WebSocketService(TerminalService terminalService, WebSocketSender webSocketSender, String websocketUrl, String endpoint) {
+    public WebSocketService(
+            WebSocketMessageHandler webSocketMessageHandler,
+            TerminalService terminalService,
+            WebSocketSender webSocketSender,
+            String websocketUrl, String endpoint)
+    {
+        this.webSocketMessageHandler = webSocketMessageHandler;
         this.terminalService = terminalService;
         this.webSocketSender = webSocketSender;
         this.websocketUrl = "ws://" + websocketUrl + endpoint;
     }
 
-    public void setWebSocketMessageHandler(WebSocketMessageHandler webSocketMessageHandler) {
-        this.webSocketMessageHandler = webSocketMessageHandler;
-    }
-
-    public boolean createSession() {
+    public boolean createSession(String sessionId) {
         ClientManager clientManager = ClientManager.createClient();
+        ClientEndpointConfig.Configurator configurator = new ClientEndpointConfig.Configurator() {
+            
+            @Override
+            public void beforeRequest(Map<String, List<String>> headers) {
+                headers.put("Cookie", List.of("SESSION=" + sessionId));
+            }
+        };
+
+        ClientEndpointConfig config = ClientEndpointConfig.Builder
+                .create().configurator(configurator).build();
+        
         try {
-            session = clientManager.connectToServer(new WebSocketMessageHandler(terminalService), new URI(websocketUrl));
+            session = clientManager.connectToServer(
+                    new WebSocketSessionHandler(terminalService, this), config, new URI(websocketUrl)
+            );
             session.addMessageHandler(webSocketMessageHandler);
+            enableKeepAlive();
             return true;
         } catch (Exception e) {
             terminalService.printSystemMessage(String.format("Failed to connect to [%s]: error: %s ", websocketUrl, e.getMessage()));
@@ -41,22 +70,58 @@ public class WebSocketService {
         }
     }
 
-    public void closeSession() {
+    public boolean closeSession() {
         try {
-            if (session != null && session.isOpen()) {
+            disableKeepAlive();
+
+            if (isSessionOpen()) {
                 session.close(new CloseReason(CloseReason.CloseCodes.NORMAL_CLOSURE, "Normal close"));
             }
             session = null;
+
+            return true;
         } catch (IOException e) {
             terminalService.printSystemMessage(String.format("Failed to close error: %s ", e.getMessage()));
+            return false;
         }
     }
 
-    public void sendMessage(Message message) {
-        if (session != null && session.isOpen()) {
-            webSocketSender.sendMessage(session, message);
+    public void sendMessage(BaseRequest baseRequest) {
+        if (isSessionOpen()) {
+            if (baseRequest instanceof MessageRequest message) {
+                webSocketSender.sendMessage(session, message);
+                return;
+            }
+
+            JsonUtil.toJson(baseRequest)
+                    .ifPresent(payload -> session.getAsyncRemote().sendText(payload, failureLoggingHandler(payload)));
         } else {
             terminalService.printSystemMessage("WebSocket is not connected.");
         }
+    }
+
+    private void enableKeepAlive() {
+        if (executorService == null)
+            executorService = Executors.newSingleThreadScheduledExecutor();
+
+        executorService.scheduleAtFixedRate(() -> sendMessage(new KeepAliveRequest()), 1, 1, TimeUnit.MINUTES);
+    }
+
+    private void disableKeepAlive() {
+        if (executorService != null) {
+            executorService.shutdown();
+            executorService = null;
+        }
+    }
+
+    private SendHandler failureLoggingHandler(String payload) {
+        return result -> {
+            if (!result.isOK())
+                terminalService.printSystemMessage("%s send failed. cause: %s".formatted(payload, result.getException()));
+        };
+    }
+
+    private boolean isSessionOpen() {
+        return session != null && session.isOpen();
     }
 }
