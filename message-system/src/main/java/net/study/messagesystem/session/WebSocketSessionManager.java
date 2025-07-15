f32cbced2ce650faaac783db7026d29dda011f6b
ksw <seonwongim@gmail.com>
2025-07-15 21:48:40 +0900

    [message-system] 1. 웹소켓 저장시 세션 아이디가 아닌 유저 아이디를 통해 저장 2. 세션 저장, 제거 메서드 이름 수정 3. 메세지 보내는 메서드 추가

--- message-system/src/main/java/net/study/messagesystem/session/WebSocketSessionManager.java
+++ message-system/src/main/java/net/study/messagesystem/session/WebSocketSessionManager.java
@@ -1,8 +1,13 @@
 package net.study.messagesystem.session;
 
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import net.study.messagesystem.dto.message.Message;
+import net.study.messagesystem.dto.user.UserId;
+import net.study.messagesystem.util.JsonUtil;
 import org.springframework.stereotype.Component;
 import org.springframework.util.function.ThrowingConsumer;
+import org.springframework.web.socket.TextMessage;
 import org.springframework.web.socket.WebSocketSession;
 
 import java.util.List;
@@ -13,22 +18,39 @@ import java.util.function.Consumer;
 
 @Slf4j
 @Component
+@RequiredArgsConstructor
 public class WebSocketSessionManager {
 
-    private final Map<String, WebSocketSession> sessions = new ConcurrentHashMap<>();
+    private final Map<UserId, WebSocketSession> sessions = new ConcurrentHashMap<>();
+    private final JsonUtil jsonUtil;
+
+    public void sendMessage(WebSocketSession session, Message message) {
+        jsonUtil.toJson(message).ifPresent(msg -> {
+            try {
+                session.sendMessage(new TextMessage(msg));
+                log.info("Send message: [{}] to {}", msg, session.getId());
+            } catch (Exception e) {
+                log.error("메세지 전송 실패. cause: {}", e.getMessage());
+            }
+        });
+    }
 
     public List<WebSocketSession> getSessions() {
         return sessions.values().stream().toList();
     }
 
-    public void storeSession(WebSocketSession session) {
-        sessions.put(session.getId(), session);
+    public WebSocketSession getSession(UserId userId) {
+        return sessions.get(userId);
+    }
+
+    public void putSession(UserId userId, WebSocketSession session) {
+        sessions.put(userId, session);
         log.info("Session stored: {}", session.getId());
     }
 
-    public void removeSession(String sessionId) {
-        log.info("Session removed: {}", sessionId);
-        Optional<WebSocketSession> webSocketSession = Optional.ofNullable(sessions.remove(sessionId));
+    public void closeSession(UserId userId) {
+        log.info("Session removed: {}", userId);
+        Optional<WebSocketSession> webSocketSession = Optional.ofNullable(sessions.remove(userId));
         webSocketSession.ifPresent(throwingConsumerWrapper(WebSocketSession::close));
     }
 
