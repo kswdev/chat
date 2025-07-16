959dc1869d84da785f44d6de5a0169f19bf0a947
ksw <seonwongim@gmail.com>
2025-07-17 00:59:08 +0900

    [message-system] message request 핸들러 추가

---
+++ message-system/src/main/java/net/study/messagesystem/handler/websocket/InviteRequestHandler.java
@@ -0,0 +1,51 @@
+package net.study.messagesystem.handler.websocket;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import net.study.messagesystem.constant.Constants;
+import net.study.messagesystem.constant.MessageType;
+import net.study.messagesystem.constant.UserConnectionStatus;
+import net.study.messagesystem.dto.user.UserId;
+import net.study.messagesystem.dto.websocket.inbound.InviteRequest;
+import net.study.messagesystem.dto.websocket.outbound.ErrorResponse;
+import net.study.messagesystem.dto.websocket.outbound.InviteNotification;
+import net.study.messagesystem.dto.websocket.outbound.InviteResponse;
+import net.study.messagesystem.service.UserConnectionService;
+import net.study.messagesystem.session.WebSocketSessionManager;
+import org.springframework.data.util.Pair;
+import org.springframework.stereotype.Component;
+import org.springframework.web.socket.WebSocketSession;
+
+import java.util.Optional;
+
+@Slf4j
+@Component
+@RequiredArgsConstructor
+public class InviteRequestHandler implements BaseRequestHandler<InviteRequest> {
+
+    private final UserConnectionService userConnectionService;
+    private final WebSocketSessionManager webSocketSessionManager;
+
+    @Override
+    public void handleRequest(WebSocketSession senderSession, InviteRequest request) {
+        UserId inviterUserId = (UserId) senderSession.getAttributes().get(Constants.USER_ID.getValue());
+        Pair<Optional<UserId>, String> result = userConnectionService.invite(inviterUserId, request.getUserInviteCode());
+
+        result.getFirst()
+                .ifPresentOrElse(partnerUserId -> {
+                    String inviterUsername = result.getSecond();
+                    webSocketSessionManager.sendMessage(senderSession, new InviteResponse(request.getUserInviteCode(), UserConnectionStatus.PENDING));
+                    webSocketSessionManager.sendMessage(
+                            webSocketSessionManager.getSession(partnerUserId), new InviteNotification(inviterUsername)
+                    );
+                }, () -> {
+                    String errorMessage = result.getSecond();
+                    webSocketSessionManager.sendMessage(senderSession, new ErrorResponse(errorMessage, MessageType.INVITE_REQUEST));
+                });
+    }
+
+    @Override
+    public Class<InviteRequest> getRequestType() {
+        return InviteRequest.class;
+    }
+}
