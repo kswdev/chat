5974203c0f739fbf2b72a37b39a47fcbd4a97222
ksw <seonwongim@gmail.com>
2025-08-06 00:14:09 +0900

    [message-system]  - RejectRequestHandler    연결 요청 거절 핸들러 추가

--- message-system/src/main/java/net/study/messagesystem/handler/websocket/RejectRequestHandler.java
+++ message-system/src/main/java/net/study/messagesystem/handler/websocket/RejectRequestHandler.java
@@ -4,19 +4,16 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import net.study.messagesystem.constant.Constants;
 import net.study.messagesystem.constant.MessageType;
+import net.study.messagesystem.constant.UserConnectionStatus;
 import net.study.messagesystem.dto.user.UserId;
-import net.study.messagesystem.dto.websocket.inbound.AcceptRequest;
 import net.study.messagesystem.dto.websocket.inbound.RejectRequest;
-import net.study.messagesystem.dto.websocket.outbound.AcceptNotification;
-import net.study.messagesystem.dto.websocket.outbound.AcceptResponse;
 import net.study.messagesystem.dto.websocket.outbound.ErrorResponse;
+import net.study.messagesystem.dto.websocket.outbound.RejectResponse;
 import net.study.messagesystem.service.UserConnectionService;
 import net.study.messagesystem.session.WebSocketSessionManager;
 import org.springframework.data.util.Pair;
 import org.springframework.stereotype.Component;
 import org.springframework.web.socket.WebSocketSession;
-
-import java.util.Optional;
 
 @Slf4j
 @Component
@@ -28,8 +25,16 @@ public class RejectRequestHandler implements BaseRequestHandler<RejectRequest> {
 
     @Override
     public void handleRequest(WebSocketSession senderSession, RejectRequest request) {
-        UserId accepterUserId = (UserId) senderSession.getAttributes().get(Constants.USER_ID.getValue());
+        UserId rejecterUserId = (UserId) senderSession.getAttributes().get(Constants.USER_ID.getValue());
+        Pair<Boolean, String> result = userConnectionService.reject(rejecterUserId, request.getUsername());
 
+        if (result.getFirst()) {
+            String inviterUsername = result.getSecond();
+            webSocketSessionManager.sendMessage(senderSession, new RejectResponse(inviterUsername, UserConnectionStatus.REJECTED));
+        } else {
+            String errorMessage = result.getSecond();
+            webSocketSessionManager.sendMessage(senderSession, new ErrorResponse(errorMessage, MessageType.REJECT_REQUEST));
+        }
     }
 
     @Override
