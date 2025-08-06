c15baf815b9cfbd8c8c467fcfa420556e5d464ac
ksw <seonwongim@gmail.com>
2025-08-06 22:40:26 +0900

    [message-system]  - RequestTypeMappingSpec    추가된 DTO 변환 시나리오 추가 및 검증

--- message-system/src/test/groovy/net/study/messagesystem/dto/RequestTypeMappingSpec.groovy
+++ message-system/src/test/groovy/net/study/messagesystem/dto/RequestTypeMappingSpec.groovy
@@ -3,8 +3,10 @@ package net.study.messagesystem.dto
 import com.fasterxml.jackson.databind.ObjectMapper
 import net.study.messagesystem.dto.websocket.inbound.AcceptRequest
 import net.study.messagesystem.dto.websocket.inbound.BaseRequest
+import net.study.messagesystem.dto.websocket.inbound.FetchUserInviteCodeRequest
 import net.study.messagesystem.dto.websocket.inbound.InviteRequest
 import net.study.messagesystem.dto.websocket.inbound.KeepAliveRequest
+import net.study.messagesystem.dto.websocket.inbound.RejectRequest
 import net.study.messagesystem.dto.websocket.inbound.WriteMessageRequest
 import net.study.messagesystem.util.JsonUtil
 import spock.lang.Specification
@@ -25,10 +27,12 @@ class RequestTypeMappingSpec extends Specification {
         validate(request)
 
         where:
-        payload                                                                     | expectedClass       | validate
-        '{"type" : "KEEP_ALIVE"}'                                                   | KeepAliveRequest    | {req -> (req as KeepAliveRequest).getType() == 'KEEP_ALIVE'}
-        '{"type" : "ACCEPT_REQUEST", "username": "inviterUsername"}'                | AcceptRequest       | {req -> (req as AcceptRequest).getUsername() == 'inviterUsername'}
-        '{"type" : "INVITE_REQUEST", "userInviteCode" : "testInviteCode123"}'       | InviteRequest       | {req -> (req as InviteRequest).userInviteCode.code() == 'testInviteCode123'}
-        '{"type" : "WRITE_MESSAGE", "username" : "sender", "content" : "테스트 내용"}' | WriteMessageRequest | {req -> (req as WriteMessageRequest).getUsername() == 'sender' && (req as WriteMessageRequest).getContent() == '테스트 내용'}
+        payload                                                                     | expectedClass              | validate
+        '{"type" : "ACCEPT_REQUEST", "username": "inviterUsername"}'                | AcceptRequest              | {req -> (req as AcceptRequest).getUsername() == 'inviterUsername'}
+        '{"type" : "FETCH_USER_INVITE_CODE_REQUEST"}'                               | FetchUserInviteCodeRequest | {req -> (req as FetchUserInviteCodeRequest).getType() == 'FETCH_USER_INVITE_CODE_REQUEST'}
+        '{"type" : "INVITE_REQUEST", "userInviteCode" : "testInviteCode123"}'       | InviteRequest              | {req -> (req as InviteRequest).userInviteCode.code() == 'testInviteCode123'}
+        '{"type" : "KEEP_ALIVE"}'                                                   | KeepAliveRequest           | {req -> (req as KeepAliveRequest).getType() == 'KEEP_ALIVE'}
+        '{"type" : "REJECT_REQUEST", "username": "inviterUsername"}'                | RejectRequest              | {req -> (req as RejectRequest).getUsername() == 'inviterUsername'}
+        '{"type" : "WRITE_MESSAGE", "username" : "sender", "content" : "테스트 내용"}' | WriteMessageRequest        | {req -> (req as WriteMessageRequest).getUsername() == 'sender' && (req as WriteMessageRequest).getContent() == '테스트 내용'}
     }
 }
