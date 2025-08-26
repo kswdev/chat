e071884983ef4b5776cec77d0a69bab4ed8acf8d
ksw <seonwongim@gmail.com>
2025-08-26 23:54:53 +0900

    [message-system]  - ChannelService  => create 메서드 생성

---
+++ message-system/src/main/java/net/study/messagesystem/service/ChannelService.java
@@ -0,0 +1,51 @@
+package net.study.messagesystem.service;
+
+import com.mysema.commons.lang.Pair;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import net.study.messagesystem.constant.ResultType;
+import net.study.messagesystem.dto.domain.channel.Channel;
+import net.study.messagesystem.dto.domain.channel.ChannelId;
+import net.study.messagesystem.dto.domain.user.UserId;
+import net.study.messagesystem.entity.channel.ChannelEntity;
+import net.study.messagesystem.entity.channel.UserChannelEntity;
+import net.study.messagesystem.repository.channel.ChannelRepository;
+import net.study.messagesystem.repository.channel.UserChannelRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Slf4j
+@Service
+@RequiredArgsConstructor
+public class ChannelService {
+
+    private final UserConnectionService userConnectionService;
+    private final UserChannelRepository userChannelRepository;
+    private final ChannelRepository channelRepository;
+
+    public Pair<Optional<Channel>, ResultType> create(UserId senderUserId, UserId participantId, String title) {
+        if (title != null && title.isEmpty()) {
+            log.warn("Invalid args: title is empty");
+            return Pair.of(Optional.empty(), ResultType.INVALID_ARGS);
+        }
+
+        try {
+            final int HEAD_COUNT = 2;
+
+            ChannelEntity channelEntity = channelRepository.save(new ChannelEntity(title, HEAD_COUNT));
+            Long channelId = channelEntity.getChannelId();
+            List<UserChannelEntity> userChannelList = List.of(new UserChannelEntity(senderUserId.id(), channelId, 0L),
+                                                              new UserChannelEntity(participantId.id(), channelId, 0L));
+
+            userChannelRepository.saveAll(userChannelList);
+
+            Channel channel = new Channel(new ChannelId(channelId), title, HEAD_COUNT);
+            return Pair.of(Optional.of(channel), ResultType.SUCCESS);
+        } catch (Exception e) {
+            log.error("Failed to create channel. cause: {}", e.getMessage());
+            throw e;
+        }
+    }
+}
