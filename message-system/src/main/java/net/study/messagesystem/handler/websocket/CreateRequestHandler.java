754990018d42642a453079d6589794da0ba3b306
ksw <seonwongim@gmail.com>
2025-08-27 11:09:00 +0900

    [message-system]  - CreateRequestHandler.java  => 채널 생성 요청 핸들러 추가

---
+++ message-system/src/main/java/net/study/messagesystem/handler/websocket/CreateRequestHandler.java
@@ -0,0 +1,73 @@
+package net.study.messagesystem.handler.websocket;
+
+import com.mysema.commons.lang.Pair;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import net.study.messagesystem.constant.IdKey;
+import net.study.messagesystem.constant.MessageType;
+import net.study.messagesystem.constant.ResultType;
+import net.study.messagesystem.dto.domain.channel.Channel;
+import net.study.messagesystem.dto.domain.user.UserId;
+import net.study.messagesystem.dto.websocket.inbound.CreateRequest;
+import net.study.messagesystem.dto.websocket.outbound.CreateResponse;
+import net.study.messagesystem.dto.websocket.outbound.ErrorResponse;
+import net.study.messagesystem.dto.websocket.outbound.JoinNotification;
+import net.study.messagesystem.service.ChannelService;
+import net.study.messagesystem.service.UserService;
+import net.study.messagesystem.session.WebSocketSessionManager;
+import org.springframework.stereotype.Component;
+import org.springframework.web.socket.WebSocketSession;
+
+import java.util.Optional;
+
+@Slf4j
+@Component
+@RequiredArgsConstructor
+public class CreateRequestHandler implements BaseRequestHandler<CreateRequest> {
+
+    private final ChannelService channelService;
+    private final UserService userService;
+    private final WebSocketSessionManager webSocketSessionManager;
+
+    @Override
+    public void handleRequest(WebSocketSession senderSession, CreateRequest request) {
+        UserId senderUserId = (UserId) senderSession.getAttributes().get(IdKey.USER_ID.getValue());
+        Optional<UserId> userId = userService.getUserId(request.getParticipantUsername());
+
+        if (userId.isEmpty()) {
+            webSocketSessionManager.sendMessage(
+                    senderSession,
+                    new ErrorResponse(ResultType.NOT_FOUND.getMessage(), MessageType.CREATE_REQUEST)
+            );
+
+            return;
+        }
+
+        UserId participantId = userId.get();
+        Pair<Optional<Channel>, ResultType> result;
+
+        try {
+            result = channelService.create(senderUserId, participantId, request.getTitle());
+        } catch (Exception e) {
+            webSocketSessionManager.sendMessage(senderSession, new ErrorResponse(e.getMessage(), MessageType.CREATE_REQUEST));
+            return;
+        }
+
+        result.getFirst()
+              .ifPresentOrElse(channel -> {
+                  webSocketSessionManager.sendMessage(senderSession, new CreateResponse(channel.channelId(), channel.title()));
+                  webSocketSessionManager.sendMessage(
+                          webSocketSessionManager.getSession(participantId), new JoinNotification(channel.channelId(), channel.title())
+                  );
+              }, () -> {
+                  String errorMessage = result.getSecond().getMessage();
+                  webSocketSessionManager.sendMessage(senderSession, new ErrorResponse(errorMessage, MessageType.CREATE_REQUEST));
+              });
+
+    }
+
+    @Override
+    public Class<CreateRequest> getRequestType() {
+        return CreateRequest.class;
+    }
+}
