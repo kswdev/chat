7a01906f486bcdfc4f46235f1cc8b8df0481b436
ksw <seonwongim@gmail.com>
2025-08-28 21:55:43 +0900

    [message-system]  - UserChannelRepository.java  => findUserIdsByChannelId 메서드 추가

--- message-system/src/main/java/net/study/messagesystem/repository/channel/UserChannelRepository.java
+++ message-system/src/main/java/net/study/messagesystem/repository/channel/UserChannelRepository.java
@@ -1,12 +1,16 @@
 package net.study.messagesystem.repository.channel;
 
+import net.study.messagesystem.dto.projection.UserIdProjection;
 import net.study.messagesystem.entity.channel.UserChannelId;
 import net.study.messagesystem.entity.channel.UserChannelEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface UserChannelRepository extends JpaRepository<UserChannelEntity, UserChannelId> {
 
     boolean existsByUserIdAndChannelId(Long userId, Long channelId);
+    List<UserIdProjection> findUserIdsByChannelId(Long channelId);
 }
