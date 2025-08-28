a3281143a537942417862c8b854d1708dde6f712
ksw <seonwongim@gmail.com>
2025-08-28 21:11:35 +0900

    [message-system]  - ChannelRepository.java  => 채널 이름 가져오는 api 추가

--- message-system/src/main/java/net/study/messagesystem/repository/channel/ChannelRepository.java
+++ message-system/src/main/java/net/study/messagesystem/repository/channel/ChannelRepository.java
@@ -1,9 +1,14 @@
 package net.study.messagesystem.repository.channel;
 
+import net.study.messagesystem.dto.projection.ChannelTitleProjection;
 import net.study.messagesystem.entity.channel.ChannelEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface ChannelRepository extends JpaRepository<ChannelEntity, Long> {
+
+    Optional<ChannelTitleProjection> findChannelTitleByChannelId(Long channelId);
 }
