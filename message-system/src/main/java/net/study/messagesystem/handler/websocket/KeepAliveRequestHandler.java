0f1264575e9dff93e25710833fffb8e60d28e67f
ksw <seonwongim@gmail.com>
2025-08-28 21:10:58 +0900

    [message-system]  - KeepAliveRequestHandler.java  => 세션 시간 연장시 레디스 생명주기도 연장

--- message-system/src/main/java/net/study/messagesystem/handler/websocket/KeepAliveRequestHandler.java
+++ message-system/src/main/java/net/study/messagesystem/handler/websocket/KeepAliveRequestHandler.java
@@ -2,6 +2,7 @@ package net.study.messagesystem.handler.websocket;
 
 import lombok.RequiredArgsConstructor;
 import net.study.messagesystem.constant.IdKey;
+import net.study.messagesystem.dto.domain.user.UserId;
 import net.study.messagesystem.dto.websocket.inbound.KeepAliveRequest;
 import net.study.messagesystem.service.SessionService;
 import org.springframework.stereotype.Component;
@@ -15,7 +16,9 @@ public class KeepAliveRequestHandler implements BaseRequestHandler<KeepAliveRequ
 
     @Override
     public void handleRequest(WebSocketSession senderSession, KeepAliveRequest request) {
+        UserId senderUserId = (UserId) senderSession.getAttributes().get(IdKey.USER_ID.getValue());
         sessionService.refreshTTL(
+                senderUserId,
                 (String) senderSession.getAttributes().get(IdKey.HTTP_SESSION_ID.getValue())
         );
     }
