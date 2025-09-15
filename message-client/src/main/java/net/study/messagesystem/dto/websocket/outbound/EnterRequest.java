c545d3444bf71c4f24051021b9225b077c62b289
ksw <seonwongim@gmail.com>
2025-09-15 21:47:54 +0900

    [message-client]  - EnterRequest.java  => 불필요한 어노테이션 삭제

--- message-client/src/main/java/net/study/messagesystem/dto/websocket/outbound/EnterRequest.java
+++ message-client/src/main/java/net/study/messagesystem/dto/websocket/outbound/EnterRequest.java
@@ -1,7 +1,5 @@
 package net.study.messagesystem.dto.websocket.outbound;
 
-import com.fasterxml.jackson.annotation.JsonCreator;
-import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Getter;
 import net.study.messagesystem.constant.MessageType;
 import net.study.messagesystem.dto.channel.ChannelId;
@@ -11,9 +9,8 @@ public class EnterRequest extends BaseRequest {
 
     private final ChannelId channelId;
 
-    @JsonCreator
     public EnterRequest(
-            @JsonProperty("channelId") ChannelId ChannelId
+            ChannelId ChannelId
     ) {
         super(MessageType.ENTER_REQUEST);
         this.channelId = ChannelId;
