7ed3cc12175eead73e9e67ce62b9307863912911
ksw <seonwongim@gmail.com>
2025-09-16 22:15:56 +0900

    [message-system]  - LeaveRequest.java  => 채널 아이디 필드가 불필요해 보여서 제거

--- message-system/src/main/java/net/study/messagesystem/dto/websocket/inbound/LeaveRequest.java
+++ message-system/src/main/java/net/study/messagesystem/dto/websocket/inbound/LeaveRequest.java
@@ -1,22 +1,14 @@
 package net.study.messagesystem.dto.websocket.inbound;
 
 import com.fasterxml.jackson.annotation.JsonCreator;
-import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Getter;
 import net.study.messagesystem.constant.MessageType;
-import net.study.messagesystem.dto.domain.channel.ChannelId;
 
 @Getter
 public class LeaveRequest extends BaseRequest {
 
-    @JsonProperty("channelId")
-    private final ChannelId channelId;
-
     @JsonCreator
-    public LeaveRequest(
-            @JsonProperty("channelId") ChannelId channelId
-    ) {
+    public LeaveRequest() {
         super(MessageType.JOIN_REQUEST);
-        this.channelId = channelId;
     }
 }
