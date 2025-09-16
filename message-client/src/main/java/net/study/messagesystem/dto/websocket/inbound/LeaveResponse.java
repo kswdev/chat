cb330c7b53952f828ca695bc912b631b80ac5df4
ksw <seonwongim@gmail.com>
2025-09-17 00:11:28 +0900

    [message-client]  - LeaveResponse.java  => @JsonCreator 추가

--- message-client/src/main/java/net/study/messagesystem/dto/websocket/inbound/LeaveResponse.java
+++ message-client/src/main/java/net/study/messagesystem/dto/websocket/inbound/LeaveResponse.java
@@ -1,11 +1,13 @@
 package net.study.messagesystem.dto.websocket.inbound;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
 import lombok.Getter;
 import net.study.messagesystem.constant.MessageType;
 
 @Getter
 public class LeaveResponse extends BaseMessage{
 
+    @JsonCreator
     public LeaveResponse() {
         super(MessageType.LEAVE_RESPONSE);
     }
