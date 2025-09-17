b07bcbbbb0fefaef7c244d2be7a4fa94891e2e1e
ksw <seonwongim@gmail.com>
2025-09-17 22:27:20 +0900

    [message-client]  - CreateRequest.java  => 채널을 만들 때 다수의 사용자를 초대할 수 있도록 username 필드를 리스트로 수정

--- message-client/src/main/java/net/study/messagesystem/dto/websocket/outbound/CreateRequest.java
+++ message-client/src/main/java/net/study/messagesystem/dto/websocket/outbound/CreateRequest.java
@@ -3,15 +3,17 @@ package net.study.messagesystem.dto.websocket.outbound;
 import lombok.Getter;
 import net.study.messagesystem.constant.MessageType;
 
+import java.util.List;
+
 @Getter
 public class CreateRequest extends BaseRequest {
 
     private final String title;
-    private final String participantUsername;
+    private final List<String> participantUsernames;
 
-    public CreateRequest(String title, String participantUsername) {
+    public CreateRequest(String title, List<String> participantUsernames) {
         super(MessageType.CREATE_REQUEST);
         this.title = title;
-        this.participantUsername = participantUsername;
+        this.participantUsernames = participantUsernames;
     }
 }
