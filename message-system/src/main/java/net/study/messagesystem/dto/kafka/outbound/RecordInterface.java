427c39a9d97d3e3ad8b370f408f6f3f5daa13d31
ksw <seonwongim@gmail.com>
2025-10-14 01:10:37 +0900

    [message-system]  - RecordInterface  => 직렬화시 type 필드 추가를 위해 json 어노테이션 추가

--- message-system/src/main/java/net/study/messagesystem/dto/kafka/outbound/RecordInterface.java
+++ message-system/src/main/java/net/study/messagesystem/dto/kafka/outbound/RecordInterface.java
@@ -1,5 +1,25 @@
 package net.study.messagesystem.dto.kafka.outbound;
 
+import com.fasterxml.jackson.annotation.JsonSubTypes;
+import com.fasterxml.jackson.annotation.JsonTypeInfo;
+import net.study.messagesystem.constant.MessageType;
+
+@JsonTypeInfo(use = JsonTypeInfo.Id.NAME, include = JsonTypeInfo.As.PROPERTY, property = "type")
+@JsonSubTypes({
+        @JsonSubTypes.Type(value = JoinResponse.class, name = MessageType.JOIN_RESPONSE),
+        @JsonSubTypes.Type(value = QuitResponse.class, name = MessageType.QUIT_RESPONSE),
+        @JsonSubTypes.Type(value = LeaveResponse.class, name = MessageType.LEAVE_RESPONSE),
+        @JsonSubTypes.Type(value = CreateResponse.class, name = MessageType.CREATE_RESPONSE),
+        @JsonSubTypes.Type(value = InviteResponse.class, name = MessageType.INVITE_RESPONSE),
+        @JsonSubTypes.Type(value = AcceptResponse.class, name = MessageType.ACCEPT_RESPONSE),
+        @JsonSubTypes.Type(value = RejectResponse.class, name = MessageType.REJECT_RESPONSE),
+        @JsonSubTypes.Type(value = DisconnectResponse.class, name = MessageType.DISCONNECT_RESPONSE),
+
+        @JsonSubTypes.Type(value = InviteNotification.class, name = MessageType.ASK_INVITE),
+        @JsonSubTypes.Type(value = JoinNotification.class, name = MessageType.NOTIFY_JOIN),
+        @JsonSubTypes.Type(value = AcceptNotification.class, name = MessageType.NOTIFY_ACCEPT),
+        @JsonSubTypes.Type(value = MessageNotification.class, name = MessageType.NOTIFY_MESSAGE),
+})
 public interface RecordInterface {
     String type();
 }
