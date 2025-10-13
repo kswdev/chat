b0374aaca06c61424292934e28f99e95ac65b040
ksw <seonwongim@gmail.com>
2025-10-14 01:05:25 +0900

    [message-push]  - PushMessageConsumer.java  => 메세지를 소비해 핸들러한테 전달

--- message-push/src/main/java/net/study/messagepush/kafka/PushMessageConsumer.java
+++ message-push/src/main/java/net/study/messagepush/kafka/PushMessageConsumer.java
@@ -1,6 +1,10 @@
 package net.study.messagepush.kafka;
 
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import net.study.messagepush.dto.kafka.inbound.RecordInterface;
+import net.study.messagepush.handler.RequestDispatcher;
+import net.study.messagepush.util.JsonUtil;
 import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.kafka.support.Acknowledgment;
@@ -8,16 +12,28 @@ import org.springframework.stereotype.Component;
 
 @Slf4j
 @Component
+@RequiredArgsConstructor
 public class PushMessageConsumer {
+
+    private final JsonUtil jsonUtil;
+    private final RequestDispatcher dispatcher;
 
     @KafkaListener(
             topics = "${message-system.kafka.listeners.push.topic}",
             groupId = "${message-system.kafka.listeners.push.group-id}",
             concurrency = "${message-system.kafka.listeners.push.concurrency}")
     public void consumeMessage(ConsumerRecord<String, String> consumerRecord, Acknowledgment acknowledgment) {
-        log.info("received record from: {} with key: {} and value: {} partition: {}, offset: {}",
-                consumerRecord.topic(), consumerRecord.key(), consumerRecord.value(), consumerRecord.partition(), consumerRecord.offset());
 
-        acknowledgment.acknowledge();
+        try {
+            log.info("received record from: {} with key: {} and value: {} partition: {}, offset: {}",
+                    consumerRecord.topic(), consumerRecord.key(), consumerRecord.value(), consumerRecord.partition(), consumerRecord.offset());
+
+            jsonUtil.fromJson(consumerRecord.value(), RecordInterface.class)
+                    .ifPresent(dispatcher::dispatch);
+        } catch (Exception e) {
+            log.error("Record handling failed. cause: {}", e.getMessage());
+        } finally {
+            acknowledgment.acknowledge();
+        }
     }
 }
