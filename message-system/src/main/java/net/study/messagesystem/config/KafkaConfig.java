e3ded08622464b3fb20beba53fc368b72f3c02a4
ksw <seonwongim@gmail.com>
2025-10-14 01:15:56 +0900

    [message-system]  - KafkaConfig.java  => 서비스 초기화시 카프카 프로듀서 초기화를 위한 빈 등록

---
+++ message-system/src/main/java/net/study/messagesystem/config/KafkaConfig.java
@@ -0,0 +1,21 @@
+package net.study.messagesystem.config;
+
+import lombok.extern.slf4j.Slf4j;
+import org.apache.kafka.clients.producer.Producer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.core.DefaultKafkaProducerFactory;
+import org.springframework.kafka.core.KafkaTemplate;
+
+@Slf4j
+@Configuration
+public class KafkaConfig {
+
+    @Bean
+    public KafkaTemplate<String, String> kafkaTemplate(DefaultKafkaProducerFactory<String, String> producerFactory) {
+        Producer<String, String> producer = producerFactory.createProducer();
+        producer.close();
+        log.info("KafkaProducer initialized.");
+        return new KafkaTemplate<>(producerFactory);
+    }
+}
