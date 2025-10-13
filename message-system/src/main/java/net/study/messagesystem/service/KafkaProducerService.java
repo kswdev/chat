4b46682f8902bebca8ea7069cd58f99a3038fee2
ksw <seonwongim@gmail.com>
2025-10-14 01:09:29 +0900

    [message-system]  - KafkaProducerService.java  => 불필요한 sendMessage 메서드 삭제

--- message-system/src/main/java/net/study/messagesystem/service/KafkaProducerService.java
+++ message-system/src/main/java/net/study/messagesystem/service/KafkaProducerService.java
@@ -7,7 +7,6 @@ import net.study.messagesystem.dto.kafka.outbound.RecordInterface;
 import net.study.messagesystem.util.JsonUtil;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.kafka.core.KafkaTemplate;
-import org.springframework.kafka.support.SendResult;
 import org.springframework.stereotype.Service;
 
 @Slf4j
@@ -21,30 +20,14 @@ public class KafkaProducerService {
     @Value("${message-system.kafka.listeners.push.topic}")
     private String pushTopic;
 
-    public void sendMessage(String topic, String key, String message) {
-        SendResult<String, String> sendResult;
-        try {
-            if (key == null || key.isEmpty())
-                sendResult = kafkaTemplate.send(topic, message).get();
-            else
-                sendResult = kafkaTemplate.send(topic, key, message).get();
-
-            log.info("sendResult: {}", sendResult);
-        } catch (Exception e) {
-            log.error("Kafka send failed. topic: {}, key: {}, message: {}", topic, key, message);
-        }
-    }
-
     public void sendPushNotification(RecordInterface recordInterface) {
         jsonUtil.toJson(recordInterface)
-                .ifPresent(record ->
-                        kafkaTemplate.send(pushTopic, record)
-                                     .whenComplete((sendResult, throwable) -> {
-                                         if (throwable != null)
-                                             log.error("Kafka send failed. record: {}, cause: {}", record, throwable.getMessage());
-                                         else
-                                             log.info("Kafka send success. topic: {}, record: {}", pushTopic, sendResult.getProducerRecord().value());
-                                     })
-        );
+                .ifPresent(record -> kafkaTemplate.send(pushTopic, record)
+                        .whenComplete((sendResult, throwable) -> {
+                            if (throwable != null)
+                                log.error("Kafka send failed. record: {}, cause: {}", record, throwable.getMessage());
+                            else
+                                log.info("Kafka send success. topic: {}, record: {}", pushTopic, sendResult.getProducerRecord().value());
+                        }));
     }
 }
