e3ec38df7bb0bfcf7465105ede80045fec77b9fa
ksw <seonwongim@gmail.com>
2025-10-20 00:55:20 +0900

    [message-system]  - 인증 의존성 추가 및 불필요한 코드 제거

--- message-system/src/main/java/net/study/messagesystem/config/SecurityConfig.java
+++ message-system/src/main/java/net/study/messagesystem/config/SecurityConfig.java
@@ -1,27 +1,13 @@
 package net.study.messagesystem.config;
 
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
 import lombok.extern.slf4j.Slf4j;
-import net.study.messagesystem.auth.RestApiLoginAuthFilter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.authentication.ProviderManager;
-import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
-import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
-
-import java.io.IOException;
 
 @Slf4j
 @Configuration
@@ -33,51 +19,16 @@ public class SecurityConfig {
     }
 
     @Bean
-    public AuthenticationManager authenticationManager(UserDetailsService detailsService, PasswordEncoder passwordEncoder) {
-        DaoAuthenticationProvider daoAuthenticationProvider = new DaoAuthenticationProvider();
-        daoAuthenticationProvider.setUserDetailsService(detailsService);
-        daoAuthenticationProvider.setPasswordEncoder(passwordEncoder);
-        return new ProviderManager(daoAuthenticationProvider);
-    }
-
-    @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity, AuthenticationManager authenticationManager) throws Exception {
-        RestApiLoginAuthFilter restApiLoginAuthFilter = new RestApiLoginAuthFilter(new AntPathRequestMatcher("/api/v1/auth/login", "POST"), authenticationManager);
+    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
 
         return httpSecurity
                 .csrf(AbstractHttpConfigurer::disable)
                 .httpBasic(AbstractHttpConfigurer::disable)
                 .formLogin(AbstractHttpConfigurer::disable)
-                .addFilterAt(restApiLoginAuthFilter, UsernamePasswordAuthenticationFilter.class)
                 .authorizeHttpRequests(auth -> auth
-                                .requestMatchers(
-                                        "/api/v1/auth/register",
-                                        "/api/v1/auth/login",
-                                        "/ws/v1/connect").permitAll()
+                                .requestMatchers("/ws/v1/connect").permitAll()
                                 .anyRequest().authenticated())
-                .logout(logout -> logout
-                                .logoutUrl("/api/v1/auth/logout")
-                                .logoutSuccessHandler(this::logoutHandler))
                 .build();
     }
 
-    private void logoutHandler(HttpServletRequest request, HttpServletResponse response, Authentication authentication) {
-        response.setContentType(MediaType.TEXT_PLAIN_VALUE);
-        response.setCharacterEncoding("UTF-8");
-        String message;
-
-        if (authentication != null && authentication.isAuthenticated()) {
-            response.setStatus(HttpStatus.OK.value());
-            message = "로그아웃 성공";
-        } else {
-            response.setStatus(HttpStatus.UNAUTHORIZED.value());
-            message = "로그아웃 실패";
-        }
-
-        try {
-            response.getWriter().write(message);
-        } catch (IOException e) {
-            log.error("전송 실패. 원인: {}", e.getMessage());
-        }
-    }
 }
