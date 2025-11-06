e693ac167abc8e7ea0ebd7fb43ab1ec588e6e9d7
ksw <seonwongim@gmail.com>
2025-11-06 23:48:36 +0900

    [message-system] - MessageRepository.java
     => MessageEntity를 복합키로 바꿔줬기 때문에 키 데이터 타입을 수정.

--- message-system/src/main/java/net/study/messagesystem/repository/MessageRepository.java
+++ message-system/src/main/java/net/study/messagesystem/repository/MessageRepository.java
@@ -1,6 +1,7 @@
 package net.study.messagesystem.repository;
 
 import net.study.messagesystem.dto.projection.MessageInfoProjection;
+import net.study.messagesystem.entity.messae.ChannelSequenceId;
 import net.study.messagesystem.entity.messae.MessageEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
@@ -11,7 +12,7 @@ import java.util.List;
 import java.util.Optional;
 
 @Repository
-public interface MessageRepository extends JpaRepository<MessageEntity, Long> {
+public interface MessageRepository extends JpaRepository<MessageEntity, ChannelSequenceId> {
 
     @Query("SELECT MAX(m.messageSequence) FROM MessageEntity m WHERE m.channelId = :channelId")
     @Transactional(readOnly = true)
