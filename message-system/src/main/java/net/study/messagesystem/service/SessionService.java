d6949770b0aaf2c4bde18f3faf857e6a6302a76e
ksw <seonwongim@gmail.com>
2025-11-18 00:27:33 +0900

    [message-system] - SessionService.java
     - getListenTopics()
     => 레디스에 저장된 유저별 위치값을 가져온다
        connection-notification:100,
        connection-notification:200
     => 유저별 위치값을 키로 유저들을 그룹핑한다

--- message-system/src/main/java/net/study/messagesystem/service/SessionService.java
+++ message-system/src/main/java/net/study/messagesystem/service/SessionService.java
@@ -2,27 +2,70 @@ package net.study.messagesystem.service;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import net.study.messagesystem.constant.KeyPrefix;
 import net.study.messagesystem.constant.IdKey;
+import net.study.messagesystem.constant.KeyPrefix;
 import net.study.messagesystem.domain.channel.ChannelId;
 import net.study.messagesystem.domain.user.UserId;
-import org.springframework.session.Session;
-import org.springframework.session.SessionRepository;
 import org.springframework.stereotype.Service;
 
-import java.time.Instant;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 @Slf4j
 @Service
 @RequiredArgsConstructor
 public class SessionService {
 
-    private final SessionRepository<? extends Session> httpSessionRepository;
     private final CacheService cacheService;
     private final long TTL = 300;
+
+    public Optional<String> getListenTopic(UserId userId) {
+        return cacheService.get(buildUserLocationKey(userId));
+    }
+
+    public Map<String, List<UserId>> getListenTopics(Collection<UserId> userIds) {
+        List<UserId> userIdList = new ArrayList<>(userIds);
+
+        List<String> keys = userIdList.stream()
+                .map(this::buildUserLocationKey)
+                .toList();
+
+        List<String> listenTopics = cacheService.get(keys);
+
+        return IntStream.range(0, userIdList.size())
+                .mapToObj(i -> Map.entry(listenTopics.get(i), userIdList.get(i)))
+                .filter(entry -> entry.getKey() != null)
+                .collect(Collectors.groupingBy(
+                        Map.Entry::getKey,
+                        Collectors.mapping(Map.Entry::getValue, Collectors.toList())
+                ));
+    }
+
+    public List<UserId> getOnlineParticipantUserIds(ChannelId channelId, List<UserId> userIds) {
+        List<String> channelIdKeys = userIds.stream()
+                .map(this::buildChannelIdKey)
+                .toList();
+
+        List<String> channelIds = cacheService.get(channelIdKeys);
+
+        if (channelIds == null || channelIds.isEmpty()) {
+            return List.of();
+        }
+
+        List<UserId> onlineParticipantUserIds = new ArrayList<>(userIds.size());
+
+        for (int i = 0; i < userIds.size(); i++) {
+            String value = channelIds.get(i);
+            onlineParticipantUserIds.add(
+                    value != null && value.equals(channelId.id().toString())
+                            ? userIds.get(i)
+                            : null
+            );
+        }
+
+        return onlineParticipantUserIds;
+    }
 
     public boolean deActiveChannel(UserId userId) {
         String channelIdKey = buildChannelIdKey(userId);
@@ -34,39 +77,11 @@ public class SessionService {
         return cacheService.set(channelIdKey, channelId.id().toString(), TTL);
     }
 
-    public List<UserId> getOnlineParticipantUserIds(ChannelId channelId, List<UserId> userIds) {
-        List<String> channelIdKeys = userIds.stream().map(this::buildChannelIdKey).toList();
-        List<String> channelIds = cacheService.get(channelIdKeys);
-
-        assert channelIds != null;
-
-        if (!channelIds.isEmpty()) {
-            List<UserId> onlineParticipantUserIds = new ArrayList<>(userIds.size());
-            for (int i = 0; i < userIds.size(); i++) {
-                String value = channelIds.get(i);
-                onlineParticipantUserIds.add(value != null && value.equals(channelId.id().toString()) ? userIds.get(i) : null);
-            }
-            return onlineParticipantUserIds;
-        }
-
-        return Collections.emptyList();
-    }
-
-    public void refreshTTL(UserId userId, String httpSessionId) {
-        String channelIdKey = buildChannelIdKey(userId);
-
-        try {
-            Session httpSession = httpSessionRepository.findById(httpSessionId);
-            if (httpSession != null) {
-                httpSession.setLastAccessedTime(Instant.now());
-                cacheService.expire(channelIdKey, TTL);
-            }
-        } catch (Exception e) {
-            log.error("Redis find failed. id: {}, cause:{}", httpSessionId, e.getMessage());
-        }
-    }
-
     private String buildChannelIdKey(UserId userId) {
         return cacheService.buildKey(KeyPrefix.USER, userId.id().toString(), IdKey.CHANNEL_ID.getValue());
+    }
+
+    private String buildUserLocationKey(UserId userId) {
+        return cacheService.buildKey(KeyPrefix.USER_SESSION, userId.id().toString());
     }
 }
