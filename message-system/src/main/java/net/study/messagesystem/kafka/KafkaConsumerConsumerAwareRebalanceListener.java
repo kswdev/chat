ac6a9bd20ebc0eacacc3c528e8e7250a7b4ae8be
ksw <seonwongim@gmail.com>
2025-11-18 00:22:11 +0900

    [message-system] - KafkaConfig.java  - kafkaAdmin()  => 카프카 토픽 자동 생성을 위한 빈 등록
     - kafkaListenerContainerFactory()
     => AckMode.MANUAL_IMMEDIATE ack.acknowledge() 를 호출해야 offset commit
    
     => Rebalance Listener 설정 Partition 재할당 시 동작하는 리스너.

---
+++ message-system/src/main/java/net/study/messagesystem/kafka/KafkaConsumerConsumerAwareRebalanceListener.java
@@ -0,0 +1,24 @@
+package net.study.messagesystem.kafka;
+
+import lombok.extern.slf4j.Slf4j;
+import org.apache.kafka.clients.consumer.Consumer;
+import org.apache.kafka.common.TopicPartition;
+import org.springframework.kafka.listener.ConsumerAwareRebalanceListener;
+import org.springframework.stereotype.Component;
+
+import java.util.Collection;
+
+@Slf4j
+@Component
+public class KafkaConsumerConsumerAwareRebalanceListener implements ConsumerAwareRebalanceListener {
+
+    @Override
+    public void onPartitionsAssigned(Consumer<?, ?> consumer, Collection<TopicPartition> partitions) {
+        log.info("kafka consumer: {}, assigned: {}", consumer.toString(), partitions.toString());
+    }
+
+    @Override
+    public void onPartitionsRevoked(Collection<TopicPartition> partitions) {
+        log.info("kafka assigned: {}",partitions.toString());
+    }
+}
