00a594b3a658ad48fa2fbafecda7c8c426e8f48c
ksw <seonwongim@gmail.com>
2025-11-18 00:39:59 +0900

    [message-system] - KafkaProducer.java
    => sendMessageUsingPartitionKey, sendResponse api 추가

--- message-system/src/main/java/net/study/messagesystem/kafka/KafkaProducer.java
+++ message-system/src/main/java/net/study/messagesystem/kafka/KafkaProducer.java
@@ -3,11 +3,16 @@ package net.study.messagesystem.kafka;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import net.study.messagesystem.domain.channel.ChannelId;
+import net.study.messagesystem.domain.user.UserId;
 import net.study.messagesystem.dto.kafka.RecordInterface;
 import net.study.messagesystem.util.JsonUtil;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.support.SendResult;
 import org.springframework.stereotype.Service;
+
+import java.util.function.BiConsumer;
 
 @Slf4j
 @Service
@@ -20,14 +25,37 @@ public class KafkaProducer {
     @Value("${message-system.kafka.listeners.push.topic}")
     private String pushTopic;
 
+    public void sendMessageUsingPartitionKey(String topic, ChannelId channelId, UserId userId, RecordInterface recordInterface, Runnable errorCallback) {
+        String partitionKey = "%d-%d".formatted(channelId.id(), userId.id());
+        jsonUtil.toJson(recordInterface)
+                .ifPresent(record -> kafkaTemplate.send(topic, partitionKey, record)
+                        .whenComplete(logResult(topic, record, partitionKey, errorCallback)));
+    }
+
+    public void sendResponse(String topic, RecordInterface recordInterface, Runnable errorCallback) {
+        jsonUtil.toJson(recordInterface)
+                .ifPresent(record -> kafkaTemplate.send(topic, record)
+                        .whenComplete(logResult(topic, record, null, errorCallback)));
+    }
+
     public void sendPushNotification(RecordInterface recordInterface) {
         jsonUtil.toJson(recordInterface)
                 .ifPresent(record -> kafkaTemplate.send(pushTopic, record)
-                        .whenComplete((sendResult, throwable) -> {
-                            if (throwable != null)
-                                log.error("Kafka send failed. record: {}, cause: {}", record, throwable.getMessage());
-                            else
-                                log.info("Kafka send success. topic: {}, record: {}", pushTopic, sendResult.getProducerRecord().value());
-                        }));
+                        .whenComplete(logResult(pushTopic, record, null, null)));
+    }
+
+    private BiConsumer<SendResult<String, String>, Throwable> logResult(String topic, String record, String partitionKey, Runnable errorCallback) {
+        return (sendResult, throwable) -> {
+            if (throwable != null) {
+                log.error("Kafka send failed. record: {} with key: {} to topic: {}, cause: {}",
+                        record, topic, partitionKey, throwable.getMessage());
+            } else {
+                log.info("Kafka send success. {}, topic: {} with key: {}",
+                        sendResult.getProducerRecord().value(), topic, partitionKey);
+
+                if (errorCallback != null)
+                    errorCallback.run();
+            }
+        };
     }
 }
