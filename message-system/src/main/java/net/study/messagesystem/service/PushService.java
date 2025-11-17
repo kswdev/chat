9c98613e57516aab31946ed8ae554becd7e0016f
ksw <seonwongim@gmail.com>
2025-11-18 00:42:40 +0900

    [message-system] - PushService.java
     - pushMessage()
     => api 인자 및 로직 수정

--- message-system/src/main/java/net/study/messagesystem/service/PushService.java
+++ message-system/src/main/java/net/study/messagesystem/service/PushService.java
@@ -2,10 +2,8 @@ package net.study.messagesystem.service;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import net.study.messagesystem.domain.user.UserId;
 import net.study.messagesystem.dto.kafka.RecordInterface;
 import net.study.messagesystem.kafka.KafkaProducer;
-import net.study.messagesystem.util.JsonUtil;
 import org.springframework.stereotype.Service;
 
 import java.util.HashMap;
@@ -15,7 +13,6 @@ import java.util.HashMap;
 @RequiredArgsConstructor
 public class PushService {
 
-    private final JsonUtil jsonUtil;
     private final KafkaProducer producerService;
     private final HashMap<String, Class<? extends RecordInterface>> pushMessageTypes = new HashMap<>();
 
@@ -23,19 +20,12 @@ public class PushService {
         this.pushMessageTypes.put(pushMessage, clazz);
     }
 
-    public void pushMessage(UserId userId, String messageType, String message) {
-        Class<? extends RecordInterface> recordInterface = pushMessageTypes.get(messageType);
-        try {
-            if (recordInterface != null) {
-                jsonUtil.addValue(message, "userId", userId.id().toString())
-                        .flatMap(json -> jsonUtil.fromJson(json, recordInterface))
-                        .ifPresent(producerService::sendPushNotification);
-                log.info("Push message: {} to user: {}", message, userId);
-            } else {
-                log.error("Invalid push message type: {}", messageType);
-            }
-        } catch (Exception e) {
-            log.error("cause: {}", e.getMessage());
+    public void pushMessage(RecordInterface recordInterface) {
+        String messageType = recordInterface.type();
+        if (pushMessageTypes.containsKey(messageType)) {
+            producerService.sendPushNotification(recordInterface);
+        } else {
+            log.error("Invalid push message type: {}", messageType);
         }
     }
 }
