f4d49171d2eb15ef67b33beb9a6c214e852ee9ae
ksw <seonwongim@gmail.com>
2025-11-19 01:55:40 +0900

    [message-push]  - MessageNotification.java  => dto 수정

--- message-push/src/main/java/net/study/messagepush/dto/kafka/inbound/MessageNotification.java
+++ message-push/src/main/java/net/study/messagepush/dto/kafka/inbound/MessageNotification.java
@@ -2,9 +2,19 @@ package net.study.messagepush.dto.kafka.inbound;
 
 import net.study.messagepush.constant.MessageType;
 import net.study.messagepush.dto.channel.ChannelId;
+import net.study.messagepush.dto.message.MessageSeqId;
 import net.study.messagepush.dto.user.UserId;
 
-public record MessageNotification(UserId userId, ChannelId channelId, String username, String content) implements RecordInterface {
+import java.util.List;
+
+public record MessageNotification(
+        UserId userId,
+        ChannelId channelId,
+        MessageSeqId messageSeqId,
+        String username,
+        String content,
+        List<UserId> participantIds
+) implements RecordInterface {
 
     @Override
     public String type() {
