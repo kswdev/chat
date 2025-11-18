ddec741df223d0c6a23368f1d0f6f065339fb394
ksw <seonwongim@gmail.com>
2025-11-19 00:00:42 +0900

    [message-system] - RequestTypeMappingSpec.groovy
     => DTO 매핑 테스트 코드 수정

--- message-system/src/test/groovy/net/study/messagesystem/dto/RequestTypeMappingSpec.groovy
+++ message-system/src/test/groovy/net/study/messagesystem/dto/RequestTypeMappingSpec.groovy
@@ -3,18 +3,17 @@ package net.study.messagesystem.dto
 import com.fasterxml.jackson.databind.ObjectMapper
 import net.study.messagesystem.constant.UserConnectionStatus
 import net.study.messagesystem.domain.channel.ChannelId
-import net.study.messagesystem.dto.websocket.inbound.AcceptRequest
-import net.study.messagesystem.dto.websocket.inbound.BaseRequest
-import net.study.messagesystem.dto.websocket.inbound.CreateRequest
-import net.study.messagesystem.dto.websocket.inbound.DisconnectRequest
-import net.study.messagesystem.dto.websocket.inbound.EnterRequest
-import net.study.messagesystem.dto.websocket.inbound.FetchChannelInviteCodeRequest
-import net.study.messagesystem.dto.websocket.inbound.FetchUserConnectionsRequest
-import net.study.messagesystem.dto.websocket.inbound.FetchUserInviteCodeRequest
-import net.study.messagesystem.dto.websocket.inbound.InviteRequest
-import net.study.messagesystem.dto.websocket.inbound.KeepAliveRequest
-import net.study.messagesystem.dto.websocket.inbound.RejectRequest
-import net.study.messagesystem.dto.websocket.inbound.WriteMessage
+import net.study.messagesystem.dto.kafka.AcceptRequestRecord
+import net.study.messagesystem.dto.kafka.CreateRequestRecord
+import net.study.messagesystem.dto.kafka.DisconnectRequestRecord
+import net.study.messagesystem.dto.kafka.EnterRequestRecord
+import net.study.messagesystem.dto.kafka.FetchChannelInviteCodeRequestRecord
+import net.study.messagesystem.dto.kafka.FetchUserConnectionsRequestRecord
+import net.study.messagesystem.dto.kafka.FetchUserInviteCodeRequestRecord
+import net.study.messagesystem.dto.kafka.InviteRequestRecord
+import net.study.messagesystem.dto.kafka.RecordInterface
+import net.study.messagesystem.dto.kafka.RejectRequestRecord
+import net.study.messagesystem.dto.kafka.WriteMessageRecord
 import net.study.messagesystem.util.JsonUtil
 import spock.lang.Specification
 
@@ -27,24 +26,23 @@ class RequestTypeMappingSpec extends Specification {
         String jsonBody = payload
 
         when:
-        BaseRequest request = jsonUtil.fromJson(jsonBody, BaseRequest).get()
+        RecordInterface recordInterface = jsonUtil.fromJson(jsonBody, RecordInterface).get()
 
         then:
-        request.getClass() == expectedClass
-        validate(request)
+        recordInterface.getClass() == expectedClass
+        validate(recordInterface)
 
         where:
-        payload                                                                                    | expectedClass                 | validate
-        '{"type" : "KEEP_ALIVE"}'                                                                  | KeepAliveRequest              | {req -> (req as KeepAliveRequest).getType() == 'KEEP_ALIVE'}
-        '{"type" : "ENTER_REQUEST", "channelId": "1"}'                                             | EnterRequest                  | {req -> (req as EnterRequest).getChannelId() == new ChannelId(1L)}
-        '{"type" : "DISCONNECT_REQUEST", "username": "username"}'                                  | DisconnectRequest             | {req -> (req as DisconnectRequest).getUsername() == 'username'}
-        '{"type" : "ACCEPT_REQUEST", "username": "inviterUsername"}'                               | AcceptRequest                 | {req -> (req as AcceptRequest).getUsername() == 'inviterUsername'}
-        '{"type" : "REJECT_REQUEST", "username": "inviterUsername"}'                               | RejectRequest                 | {req -> (req as RejectRequest).getUsername() == 'inviterUsername'}
-        '{"type" : "INVITE_REQUEST", "userInviteCode" : "testInviteCode123"}'                      | InviteRequest                 | {req -> (req as InviteRequest).getUserInviteCode().code() == 'testInviteCode123'}
-        '{"type" : "WRITE_MESSAGE", "channelId" : "1", "content" : "테스트 내용", "serial" : "1"}'    | WriteMessage                  | {req -> (req as WriteMessage).getChannelId() == new ChannelId(1L) && (req as WriteMessage).getContent() == '테스트 내용'}
-        '{"type" : "CREATE_REQUEST", "title": "채널 생성", "participantUsernames": ["kim", "hong"]}' | CreateRequest                 | {req -> (req as CreateRequest).getTitle() == "채널 생성" && (req as CreateRequest).getParticipantUsernames().size() == 2}
-        '{"type" : "FETCH_USER_INVITE_CODE_REQUEST"}'                                              | FetchUserInviteCodeRequest    | {req -> (req as FetchUserInviteCodeRequest).getType() == 'FETCH_USER_INVITE_CODE_REQUEST'}
-        '{"type" : "FETCH_USER_CONNECTIONS_REQUEST", "status": "PENDING"}'                         | FetchUserConnectionsRequest   | {req -> (req as FetchUserConnectionsRequest).getStatus() == UserConnectionStatus.PENDING}
-        '{"type" : "FETCH_CHANNEL_INVITE_CODE_REQUEST", "channelId": "1"}'                         | FetchChannelInviteCodeRequest | {req -> (req as FetchChannelInviteCodeRequest).getChannelId() == new ChannelId(1L)}
+        payload                                                                                  | expectedClass                       | validate
+        '{"type" : "ENTER_REQUEST", "channelId": "1"}'                                           | EnterRequestRecord                  | { req -> (req as EnterRequestRecord).channelId() == new ChannelId(1L) }
+        '{"type" : "DISCONNECT_REQUEST", "username": "username"}'                                | DisconnectRequestRecord             | { req -> (req as DisconnectRequestRecord).username() == 'username' }
+        '{"type" : "ACCEPT_REQUEST", "username": "inviterUsername"}'                             | AcceptRequestRecord                 | { req -> (req as AcceptRequestRecord).username() == 'inviterUsername' }
+        '{"type" : "REJECT_REQUEST", "username": "inviterUsername"}'                             | RejectRequestRecord                 | { req -> (req as RejectRequestRecord).username() == 'inviterUsername' }
+        '{"type" : "INVITE_REQUEST", "userInviteCode" : "testInviteCode123"}'                    | InviteRequestRecord                 | { req -> (req as InviteRequestRecord).userInviteCode().code() == 'testInviteCode123' }
+        '{"type" : "WRITE_MESSAGE", "channelId" : "1", "content" : "테스트 내용", "serial" : "1"}'  | WriteMessageRecord                | { req -> (req as WriteMessageRecord).channelId() == new ChannelId(1L) && (req as WriteMessageRecord).content() == '테스트 내용' }
+        '{"type" : "CREATE_REQUEST", "title": "채널 생성", "participantUsernames": ["kim", "hong"]}'| CreateRequestRecord               | { req -> (req as CreateRequestRecord).title() == "채널 생성" && (req as CreateRequestRecord).participantUsernames().size() == 2 }
+        '{"type" : "FETCH_USER_INVITE_CODE_REQUEST"}'                                            | FetchUserInviteCodeRequestRecord    | { req -> (req as FetchUserInviteCodeRequestRecord).type() == 'FETCH_USER_INVITE_CODE_REQUEST' }
+        '{"type" : "FETCH_USER_CONNECTIONS_REQUEST", "status": "PENDING"}'                       | FetchUserConnectionsRequestRecord   | { req -> (req as FetchUserConnectionsRequestRecord).status() == UserConnectionStatus.PENDING }
+        '{"type" : "FETCH_CHANNEL_INVITE_CODE_REQUEST", "channelId": "1"}'                       | FetchChannelInviteCodeRequestRecord | { req -> (req as FetchChannelInviteCodeRequestRecord).channelId() == new ChannelId(1L) }
     }
 }
