c54eb5479c916b6197ca070c9313def27d53c775
ksw <seonwongim@gmail.com>
2025-11-19 01:48:38 +0900

    [docker]  - prepare_topics.sh  => kafka_topics.sh 쉘 경로 수정

--- docker/prepare_topics.sh
+++ docker/prepare_topics.sh
@@ -1,19 +1,20 @@
 #!/bin/bash
 
-docker exec -it chat-kafka1-1 kafka-topics.sh --create \
+docker exec -it chat-kafka1-1 /opt/kafka/bin/kafka-topics.sh --create \
   --bootstrap-server localhost:9092 \
   --topic push-notification \
   --partitions 6 \
   --replication-factor 3
 
-docker exec -it chat-kafka1-1 kafka-topics.sh --create \
+docker exec -it chat-kafka1-1 /opt/kafka/bin/kafka-topics.sh --create \
   --bootstrap-server localhost:9092 \
   --topic message-relay \
   --partitions 6 \
   --replication-factor 3
 
-docker exec -it chat-kafka1-1 kafka-topics.sh --create \
+docker exec -it chat-kafka1-1 /opt/kafka/bin/kafka-topics.sh --create \
   --bootstrap-server localhost:9092 \
-  --topic message-reqeust \
+  --topic message-request \
   --partitions 6 \
   --replication-factor 3
+
