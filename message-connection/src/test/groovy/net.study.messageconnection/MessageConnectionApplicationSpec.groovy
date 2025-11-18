86ef3b24cc7c49a1e559ef9cb270c9917fb74ffb
ksw <seonwongim@gmail.com>
2025-11-19 01:45:47 +0900

    [message-connection] - MessageConnectionApplicationSpec.groovy
     - 테스트 환경변수 추가

--- message-connection/src/test/groovy/net.study.messageconnection/MessageConnectionApplicationSpec.groovy
+++ message-connection/src/test/groovy/net.study.messageconnection/MessageConnectionApplicationSpec.groovy
@@ -2,9 +2,11 @@ package net.study.messageconnection
 
 
 import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.TestPropertySource
 import spock.lang.Specification
 
 @SpringBootTest(classes = MessageConnectionApplication)
+@TestPropertySource(properties = "server.id=test")
 class MessageConnectionApplicationSpec extends Specification{
 
     void contextLoads() {
