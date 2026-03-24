91db1ace47eef8fe04ec98afbfa4a45c9b603cb2
ksw <seonwongim@gmail.com>
2026-03-25 00:47:10 +0900

    [web-gateway]  - nginx 대신 할 게이트웨이 서비스 추가

---
+++ web-gateway/src/main/java/net/study/webgateway/common/filter/LoggingFilter.java
@@ -0,0 +1,47 @@
+package net.study.webgateway.common.filter;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.cloud.gateway.filter.GatewayFilter;
+import org.springframework.cloud.gateway.filter.factory.AbstractGatewayFilterFactory;
+import org.springframework.http.server.reactive.ServerHttpRequest;
+import org.springframework.http.server.reactive.ServerHttpResponse;
+import org.springframework.stereotype.Component;
+import reactor.core.publisher.Mono;
+
+@Slf4j
+@Component
+public class LoggingFilter extends AbstractGatewayFilterFactory<LoggingFilter.Config> {
+
+
+    public LoggingFilter() {
+        super(Config.class);
+    }
+
+    @Override
+    public GatewayFilter apply(Config config) {
+        return (exchange, chain) -> {
+
+            ServerHttpRequest request = exchange.getRequest();
+            ServerHttpResponse response = exchange.getResponse();
+
+            log.info("Logging Filter baseMessage: {}", config.getBaseMessage());
+
+            if (config.isPreLogger())
+                log.info("Logging Post filter start: request URI -> {}", request.getURI());
+
+            return chain.filter(exchange).then(Mono.fromRunnable(() -> {
+                log.info("Logging Post filter end: response code -> {}", response.getStatusCode());
+            }));
+        };
+    }
+
+    @Getter
+    @AllArgsConstructor
+    public static class Config {
+        private String baseMessage;
+        private boolean preLogger;
+        private boolean postLogger;
+    }
+}
