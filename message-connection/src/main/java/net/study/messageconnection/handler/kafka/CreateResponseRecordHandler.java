d8e89039d596397030935763dcb7f3cb6a4a4ef3
ksw <seonwongim@gmail.com>
2026-03-24 23:03:37 +0900

    [message-connection]  - 불필요한 임포트 제거

--- message-connection/src/main/java/net/study/messageconnection/handler/kafka/CreateResponseRecordHandler.java
+++ message-connection/src/main/java/net/study/messageconnection/handler/kafka/CreateResponseRecordHandler.java
@@ -2,9 +2,7 @@ package net.study.messageconnection.handler.kafka;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import net.study.messageconnection.dto.kafka.AcceptNotificationRecord;
 import net.study.messageconnection.dto.kafka.CreateResponseRecord;
-import net.study.messageconnection.dto.websocket.outbound.AcceptNotification;
 import net.study.messageconnection.dto.websocket.outbound.CreateResponse;
 import net.study.messageconnection.service.ClientNotificationService;
 import org.springframework.stereotype.Component;
