31c66170a1350d33bd0c373413ee08cae2ad1699
ksw <seonwongim@gmail.com>
2026-03-30 20:17:42 +0900

    [message-auth]  - SecurityConfig.securityFilterChain  => 불필요한 매처 제거

--- message-auth/src/main/java/net/study/messageauth/config/SecurityConfig.java
+++ message-auth/src/main/java/net/study/messageauth/config/SecurityConfig.java
@@ -59,8 +59,7 @@ public class SecurityConfig {
                 .authorizeHttpRequests(auth -> auth
                                 .requestMatchers(
                                         "/api/v1/auth/register",
-                                        "/api/v1/auth/login",
-                                        "/ws/v1/connect").permitAll()
+                                        "/api/v1/auth/login").permitAll()
                                 .anyRequest().authenticated())
                 .sessionManagement(auth -> auth.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .logout(logout -> logout
