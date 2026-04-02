5431f9a76a25630ad00f4663f9478059b6796453
ksw <seonwongim@gmail.com>
2026-04-02 23:32:05 +0900

    [message-connection-flux] => webflux 기반 웹소켓 서비스 이관을 위한 초기 프로젝트 세팅 추가

---
+++ message-connection-flux/src/main/java/net/study/messageconnectionflux/MessageConnectionFluxApplication.java
@@ -0,0 +1,13 @@
+package net.study.messageconnectionflux;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MessageConnectionFluxApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MessageConnectionFluxApplication.class, args);
+    }
+
+}
---
+++ message-connection-flux/src/test/groovy/net/study/messageconnectionflux/MessageConnectionFluxApplicationSpec.groovy
@@ -0,0 +1,13 @@
+package net.study.messageconnectionflux;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MessageConnectionFluxApplicationSpec {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
