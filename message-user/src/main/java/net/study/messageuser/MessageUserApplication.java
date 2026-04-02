2f9d7a4685ad67f1aaa13b6133ce51b6efc6e636
ksw <seonwongim@gmail.com>
2026-04-02 23:31:08 +0900

    [message-user] => 메인 테스트 및 클래스 추가

---
+++ message-user/src/main/java/net/study/messageuser/MessageUserApplication.java
@@ -0,0 +1,13 @@
+package net.study.messageuser;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MessageUserApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MessageUserApplication.class, args);
+    }
+
+}
---
+++ message-user/src/test/java/net/study/messageuser/MessageUserApplicationTests.java
@@ -0,0 +1,13 @@
+package net.study.messageuser;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MessageUserApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
