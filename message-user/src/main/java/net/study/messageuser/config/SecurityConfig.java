e873e0440742877698d1dd295fe98c3910cf14e6
ksw <seonwongim@gmail.com>
2026-09-04 23:29:47 +0900

    [message-user]  - 부하 테스트시 암호화 사용하지 않도록 수정

--- message-user/src/main/java/net/study/messageuser/config/SecurityConfig.java
+++ message-user/src/main/java/net/study/messageuser/config/SecurityConfig.java
@@ -1,7 +1,9 @@
 package net.study.messageuser.config;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Profile;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Component;
 
@@ -9,8 +11,15 @@ import org.springframework.stereotype.Component;
 public class SecurityConfig {
 
     @Bean
+    @Profile("!load-test")
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
     }
 
+    @Bean
+    @Profile("load-test")
+    @SuppressWarnings("deprecation")
+    public PasswordEncoder loadTestPasswordEncoder() {
+        return NoOpPasswordEncoder.getInstance();
+    }
 }
