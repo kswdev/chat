e81955f41127d53c1786df9c6642b5c25e1a9db8
ksw <seonwongim@gmail.com>
2026-09-11 16:13:11 +0900

    [message-social]  - message-system 으로부터 메세지 송수신을 제외한 다른 이벤트를 분리하기 위해 생성

---
+++ message-social/src/main/java/net/study/messagesocial/MessageSocialApplication.java
@@ -0,0 +1,13 @@
+package net.study.messagesocial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MessageSocialApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MessageSocialApplication.class, args);
+    }
+
+}
---
+++ message-social/src/test/java/net/study/messagesocial/MessageSocialApplicationTests.java
@@ -0,0 +1,13 @@
+package net.study.messagesocial;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MessageSocialApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
