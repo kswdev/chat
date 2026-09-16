e13817ff1ae460b5346fe3dfaff4a78a33d1f7fc
ksw <seonwongim@gmail.com>
2026-09-16 21:05:52 +0900

    [message-social]  - FriendController  => 친구 초대 api 검증 및 추가

---
+++ message-social/src/main/java/net/study/messagesocial/adapter/in/web/dto/request/InviteRequest.java
@@ -0,0 +1,3 @@
+package net.study.messagesocial.adapter.in.web.dto.request;
+
+public record InviteRequest(String userInviteCode) { }
---
+++ message-social/src/main/java/net/study/messagesocial/adapter/in/web/dto/response/InviteResponse.java
@@ -0,0 +1,3 @@
+package net.study.messagesocial.adapter.in.web.dto.response;
+
+public record InviteResponse(Long userId, String inviteCode, String status) { }
