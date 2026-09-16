f921e68a64f844e7a81c7a0613688794c193ee03
ksw <seonwongim@gmail.com>
2026-09-16 21:33:03 +0900

    [message-social]  - FriendController  => 불필요한 import 제거

--- message-social/src/test/java/net/study/messagesocial/adapter/in/web/api/FriendControllerTest.java
+++ message-social/src/test/java/net/study/messagesocial/adapter/in/web/api/FriendControllerTest.java
@@ -4,7 +4,6 @@ import net.study.messagecommon.constant.IdKey;
 import net.study.messagesocial.application.port.in.FriendInvite;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
