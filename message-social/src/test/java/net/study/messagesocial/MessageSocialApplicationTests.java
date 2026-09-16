35bcdc7e13f10cd3577378b7980bb94175c45db9
ksw <seonwongim@gmail.com>
2026-09-16 16:37:02 +0900

    [message-social]  - 헤더 필터 검증 및 구현

--- message-social/src/test/java/net/study/messagesocial/MessageSocialApplicationTests.java
+++ message-social/src/test/java/net/study/messagesocial/MessageSocialApplicationTests.java
@@ -1,10 +1,12 @@
 package net.study.messagesocial;
 
+import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class MessageSocialApplicationTests {
 
+    @Test
     void contextLoads() {
 
     }
