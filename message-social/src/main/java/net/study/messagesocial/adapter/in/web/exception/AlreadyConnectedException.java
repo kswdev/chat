bfe0dcf181008701d430bf5a4b3736295175a533
ksw <seonwongim@gmail.com>
2026-09-22 14:22:48 +0900

    [message-social]  - invite 애러 추가

--- message-social/src/main/java/net/study/messagesocial/adapter/in/web/exception/AlreadyConnectedException.java
+++ message-social/src/main/java/net/study/messagesocial/adapter/in/web/exception/AlreadyConnectedException.java
@@ -1,15 +1,10 @@
 package net.study.messagesocial.adapter.in.web.exception;
 
-import lombok.Getter;
 import net.study.messagesocial.adapter.in.web.dto.error.ErrorCode;
 
-public class AlreadyConnectedException extends RuntimeException {
-
-    @Getter
-    private final ErrorCode errorCode;
+public class AlreadyConnectedException extends FriendException {
 
     public AlreadyConnectedException(ErrorCode errorCode) {
-        super(errorCode.getMessage());
-        this.errorCode = errorCode;
+        super(errorCode);
     }
 }
--- message-social/src/main/java/net/study/messagesocial/adapter/in/web/exception/AlreadyInvitedException.java
+++ message-social/src/main/java/net/study/messagesocial/adapter/in/web/exception/AlreadyInvitedException.java
@@ -1,15 +1,10 @@
 package net.study.messagesocial.adapter.in.web.exception;
 
-import lombok.Getter;
 import net.study.messagesocial.adapter.in.web.dto.error.ErrorCode;
 
-public class AlreadyInvitedException extends RuntimeException {
-
-    @Getter
-    private final ErrorCode errorCode;
+public class AlreadyInvitedException extends FriendException {
 
     public AlreadyInvitedException(ErrorCode errorCode) {
-        super(errorCode.getMessage());
-        this.errorCode = errorCode;
+        super(errorCode);
     }
 }
